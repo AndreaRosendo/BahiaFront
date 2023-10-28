90d19fddb26f22bd0ae3fdcb11359d7e6d0bcd9d
Andrea <144391051+AndreaRosendo@users.noreply.github.com>
2023-10-28 02:50:40 -0300

    Add files via upload

--- JS/arch-java.js
+++ JS/arch-java.js
@@ -40,64 +40,70 @@ function BotonDark() {
     card.style.color = "#FFF"; //
     card.style.borderColor = "#CC0000";
   });
-  
+
+  const nuevaImagenURL = "url(../Imagenes/Mapamundi-negro.png)";
+  document.body.style.backgroundImage = nuevaImagenURL;
+
   document.querySelector("footer").style.backgroundColor = "#FFF";
   document.querySelector("footer").forEach((p) => {
     p.style.color = "black";
   });
 
-  }
+}
 
 
 function BotonWhite() {
 
-document.querySelector("body").style.backgroundColor = "#FFFF99";
-document.querySelector("body").style.color = "black";
-document.querySelector("header").style.backgroundColor = "#FF9933";
-document.querySelector("header").style.color = "#A93226";
-document.querySelector("nav").style.backgroundColor = "black";
-document.querySelector("nav").style.color = "#FFF";
-document.querySelectorAll("a").forEach((a) => {
-  a.style.color = "#FFF";
-});
-document.querySelectorAll("p").forEach((p) => {
-  p.style.color = "black";
-});
-document.querySelectorAll("label").forEach((label) => {
-  label.style.color = "black";
-});
-document.querySelectorAll("input").forEach((input) => {
-  input.style.color = "black";
-});
-document.querySelectorAll("li").forEach((li) => {
-  li.style.color = "black";
-});
-document.querySelector("h1").style.color = "#A93226";
-document.querySelectorAll("h2").forEach((h2) => {
-  h2.style.color = "#196F3D";
-});
-document.querySelectorAll("h3").forEach((h3) => {
-  h3.style.color = "#CC0000";
-});
-document.querySelectorAll("h4").forEach((h4) => {
-  h4.style.color = "black";
-});
-document.querySelectorAll("h5").forEach((h5) => {
-  h5.style.color = "black";
-});
+  document.querySelector("body").style.backgroundColor = "#FFFF99";
+  document.querySelector("body").style.color = "black";
+  document.querySelector("header").style.backgroundColor = "#FF9933";
+  document.querySelector("header").style.color = "#A93226";
+  document.querySelector("nav").style.backgroundColor = "black";
+  document.querySelector("nav").style.color = "#FFF";
+  document.querySelectorAll("a").forEach((a) => {
+    a.style.color = "#FFF";
+  });
+  document.querySelectorAll("p").forEach((p) => {
+    p.style.color = "black";
+  });
+  document.querySelectorAll("label").forEach((label) => {
+    label.style.color = "black";
+  });
+  document.querySelectorAll("input").forEach((input) => {
+    input.style.color = "black";
+  });
+  document.querySelectorAll("li").forEach((li) => {
+    li.style.color = "black";
+  });
+  document.querySelector("h1").style.color = "#A93226";
+  document.querySelectorAll("h2").forEach((h2) => {
+    h2.style.color = "#196F3D";
+  });
+  document.querySelectorAll("h3").forEach((h3) => {
+    h3.style.color = "#CC0000";
+  });
+  document.querySelectorAll("h4").forEach((h4) => {
+    h4.style.color = "black";
+  });
+  document.querySelectorAll("h5").forEach((h5) => {
+    h5.style.color = "black";
+  });
 
-let cards = document.querySelectorAll(".card");
-cards.forEach(card => {
-  card.style.backgroundColor = "#FFFFCC";
-  card.style.color = "black";
-  card.style.borderColor = "#CC0000";
-});
+  let cards = document.querySelectorAll(".card");
+  cards.forEach(card => {
+    card.style.backgroundColor = "#FFFFCC";
+    card.style.color = "black";
+    card.style.borderColor = "#CC0000";
+  });
 
-document.querySelector("footer").style.backgroundColor = "black";
-document.querySelector("footer").forEach((p) => {
-  p.style.color = "#FFF";
-});
+  const nuevaImagenURL = "url(../Imagenes/Mapamundi-blanco.png)";
+  document.body.style.backgroundImage = nuevaImagenURL;
+
+  document.querySelector("footer").style.backgroundColor = "black";
+  document.querySelector("footer").forEach((p) => {
+    p.style.color = "#FFF";
+  });
 
 }
 
-function Cartelito() {alert("Creado por Andrea Rosendo - Para el proyecto de Potrero Digital Bahía Blanca - Buenos Aires - Argentina")}
+function Cartelito() { alert("Creado por Andrea Rosendo - Para el proyecto de Potrero Digital Bahía Blanca - Buenos Aires - Argentina") }
